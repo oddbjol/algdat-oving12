cffcb1396bb1257abd446abb397c60e351208211
odd <oddbjool@stud.ntnu.no>
2017-11-11 16:16:47 +0100

    Binary storing of compressed data, still not working for utf8...

--- huffman/compress.js
+++ huffman/compress.js
@@ -3,7 +3,26 @@ let fs = require("fs");
 let argv = process.argv.slice(2);
 
 
-let data = fs.readFileSync(argv[0],'utf8');
+let data = fs.readFileSync(argv[0],'ascii');
 let huffman = new Huffman();
 let compressed = huffman.huffmanCode(data);
-fs.writeFileSync(argv[1],JSON.stringify(compressed),'utf8');
+
+fs.open(argv[1],"w", function(error, fd){
+    "use strict";
+    let buffer = Buffer.alloc(256 * 4 + compressed.data.length,0,'binary');
+    for(let i = 0; i < 256; i++) {
+        let charNum = compressed.frequencies[String.fromCharCode(i)];
+        if(charNum > 0)
+            buffer.writeInt32LE(charNum,i*4);
+        else
+            buffer.writeInt32LE(0,i*4);
+    }
+    buffer.write(compressed.data,256*4,compressed.data.length,'binary');
+
+    fs.writeSync(fd, buffer, 0, buffer.length,0);
+
+
+});
+
+
+//fs.writeFileSync(argv[1],JSON.stringify(compressed),'utf8');
--- huffman/decompress.js
+++ huffman/decompress.js
@@ -1,25 +1,23 @@
-let Huffman = require("./Huffman");
 let fs = require("fs");
+let Huffman = require("./Huffman");
 let argv = process.argv.slice(2);
 
-//
-// fs.stat(argv[0],function(error, stats){
-//     "use strict";
-//
-//     fs.open(argv[0],"r", function(error, fd){
-//         "use strict";
-//         let buffer = new Buffer(stats.size);
-//         fs.readSync(fd, buffer, 0, )
-//
-//
-//     });
-//
-// });
+fs.stat(argv[0],function(error, stats){
+    "use strict";
 
+    fs.open(argv[0],"r", function(error, fd){
+        "use strict";
+        let buffer = new Buffer(stats.size);
+        fs.readSync(fd, buffer, 0, stats.size, 0);
+        let frequencies = {};
+        for(let i = 0; i < 256; i++){
+            let numChar = buffer.readInt32LE(i*4);
+            if(numChar > 0)
+                frequencies[String.fromCharCode(i)] = numChar;
+        }
+        let data = buffer.toString('binary',256*4);
+        let decompressed = new Huffman().huffmanDecode(data, frequencies);
+        fs.writeFileSync(argv[1],decompressed,'utf8');
+    });
 
-let raw = fs.readFileSync(argv[0],'utf8');
-let compressed = JSON.parse(raw);
-let huffman = new Huffman();
-let decompressed = huffman.huffmanDecode(compressed.data, compressed.frequencies);
-
-fs.writeFileSync(argv[1],decompressed,'utf8');
+});
