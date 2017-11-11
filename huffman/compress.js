8c41d166cde7c7b1719199889ad7cf8ac0b006f6
odd <oddbjool@stud.ntnu.no>
2017-11-11 15:11:17 +0100

    begin change from utf to ascii

--- huffman/compress.js
+++ huffman/compress.js
@@ -6,4 +6,4 @@ let argv = process.argv.slice(2);
 let data = fs.readFileSync(argv[0],'utf8');
 let huffman = new Huffman();
 let compressed = huffman.huffmanCode(data);
-fs.writeFileSync(argv[1],JSON.stringify(compressed));
+fs.writeFileSync(argv[1],JSON.stringify(compressed),'utf8');
--- huffman/decompress.js
+++ huffman/decompress.js
@@ -2,9 +2,24 @@ let Huffman = require("./Huffman");
 let fs = require("fs");
 let argv = process.argv.slice(2);
 
+//
+// fs.stat(argv[0],function(error, stats){
+//     "use strict";
+//
+//     fs.open(argv[0],"r", function(error, fd){
+//         "use strict";
+//         let buffer = new Buffer(stats.size);
+//         fs.readSync(fd, buffer, 0, )
+//
+//
+//     });
+//
+// });
+
 
 let raw = fs.readFileSync(argv[0],'utf8');
 let compressed = JSON.parse(raw);
 let huffman = new Huffman();
 let decompressed = huffman.huffmanDecode(compressed.data, compressed.frequencies);
-fs.writeFileSync(argv[1],decompressed);
+
+fs.writeFileSync(argv[1],decompressed,'utf8');
